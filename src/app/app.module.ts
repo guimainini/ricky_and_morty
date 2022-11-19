7676884e6d80dac2fa820eee69f76053225b04ba
Guillermo Mainini <guimainini@gmail.com>
2022-11-19 16:17:11 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CharactersListModule } from './components/pages/characters/characters-list/characters-list.module';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderModule } from './shared/components/header/header.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CharactersListModule,
+    GraphQLModule,
+    HttpClientModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pages/characters/characters-details/characters-details.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharactersDetailsComponent } from './characters-details.component';
+
+
+
+@NgModule({
+  declarations: [
+    CharactersDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CharactersDetailsModule { }
